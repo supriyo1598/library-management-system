b3b773055492779c8e73f8859f3f1db6904ebd43
supriyo1598 <supriyosarkarcome@gmail.com>
2021-01-07 23:50:57 +0530

    done

--- Library Management System/src/library/management/system/ReadMe.java
+++ Library Management System/src/library/management/system/ReadMe.java
@@ -32,25 +32,7 @@ public class ReadMe extends JFrame{
             l4.setBounds(70, 90, 405, 40);
             contentPane.add(l4);
 
-            JLabel l5 = new JLabel("v5.1");
-            l5.setForeground(new Color(30, 144, 255));
-            l5.setFont(new Font("Trebuchet MS", Font.BOLD, 25));
-            l5.setBounds(185, 140, 100, 21);
-            contentPane.add(l5);
 
-
-            JLabel l6 = new JLabel("Developed By : Code for Interview");
-            l6.setFont(new Font("Trebuchet MS", Font.BOLD, 30));
-            l6.setBounds(70, 198, 600, 35);
-            contentPane.add(l6);
-
-            JTextArea l7 = new JTextArea("Library management system is a project which\n aims in developing a computerized system to\n maintain all the daily work of a library. The system helps\n both students and library manager to keep a constant track\n of all the books available in the library. It allows both the\n admin and the student to search for the desired book.");
-            l7.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
-            l7.setBounds(70, 260, 600, 200);
-            contentPane.add(l7);
-
-                
-                
             contentPane.setBackground(Color.WHITE);
 	}
         
